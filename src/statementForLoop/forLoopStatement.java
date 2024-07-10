713768f16ddd70b6f49d49a64243290da375f2b4
armindroid <l.sanchez3024@gmail.com>
2024-07-09 23:44:02 -0600

    feature: Se agrego el codigo para la practica del uso del for loop

--- src/statementForLoop/forLoopStatement.java
+++ src/statementForLoop/forLoopStatement.java
@@ -2,6 +2,25 @@ package statementForLoop;
 
 public class forLoopStatement {
     public static void main(String[] args) {
+        int countPrimeNumbers = 0;
+        //Haciendo uso de la sentencia for para checar los primeros 1000 numeros
+        for (int i = 10; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("The number " + i + " is a prime number");
+                countPrimeNumbers++;
+                if (countPrimeNumbers == 3) {
+                    break;
+                }
+            }
+        }
+    }
 
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
